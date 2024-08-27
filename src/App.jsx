09965395801073229d2be715007f8d3240514175
CodeMuscle <investportal99@gmail.com>
2024-08-27 08:22:58 +0530

    About Us Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <div className="overflow-y-hidden bg-orange-50 text-emarald-950 antialiased ">
+    <div className="overflow-y-hidden bg-orange-50 text-emerald-950 antialiased">
       <Navbar />
+      <Hero />
     </div>
-  )
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
-import logo from '../../public/assets/logo.png';
+
+import { FaBars, FaTimes } from "react-icons/fa";
+import logo from "../assets/logo.png";
+import { LINKS } from "../constants/index"
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -29,14 +32,15 @@ const Navbar = () => {
     setIsOpen(false);
     const offset = -70;
     const element = document.getElementById(id);
-    const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+    const elementPosition =
+      element.getBoundingClientRect().top + window.scrollY;
     const offsetPosition = elementPosition + offset;
 
     window.scrollTo({
       top: offsetPosition,
       behavior: "smooth",
-    })
-  }
+    });
+  };
 
   return (
     <>
@@ -49,15 +53,36 @@ const Navbar = () => {
               </a>
             </div>
             <div>
-              <button className="inline-flex items-center justify-center bg-emerald-950 p-2 text-orange-50" type="button" onClick={() => setIsOpen(!isOpen)}>
+              <button
+                className="inline-flex items-center justify-center bg-emerald-950 p-2 text-orange-50"
+                type="button"
+                onClick={() => setIsOpen(!isOpen)}
+              >
                 <FaBars className="h-6 w-6" />
               </button>
             </div>
           </div>
         </div>
       </nav>
+
+      {isOpen && (
+        <div className="fixed inset-0 z-20 flex flex-col space-y-8 bg-emerald-950 px-20 pt-20 text-5xl font-bold uppercase text-emerald-100 lg:text-6xl">
+          <button
+            className="absolute right-4 top-4 rounded-full bg-emerald-900 p-2 tex-orange-50 lg:right-20"
+            type="button"
+            onClick={() => setIsOpen(false)}
+          >
+            <FaTimes className="h-8 w-8" />
+          </button>
+          {LINKS.map((link) => (
+            <a href={link.id} key={link.id} className="transition-colors duration-500 hover:text-orange-500" onClick={() => handleLinkClick(link.id)}>
+              {link.name}
+            </a>
+          ))}
+        </div>
+      )}
     </>
-  )
+  );
 };
 
 export default Navbar;
