6fcac370113c549d74ab0afb3956f1e1a6d059cc
CodeMuscle <investportal99@gmail.com>
2024-08-27 08:23:59 +0530

    About Us Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import About from "./components/About";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Services from "./components/Services";
 
 export default function App() {
   return (
     <div className="overflow-y-hidden bg-orange-50 text-emerald-950 antialiased">
       <Navbar />
       <Hero />
+      <About />
+      <Services />
     </div>
   );
 }
